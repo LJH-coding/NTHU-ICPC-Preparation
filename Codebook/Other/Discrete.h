8829b92d84f5c6791d3ff9d7ff891031f51eb6dd
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-02-02 04:10:07 +0800

    first commit

---
+++ Codebook/Other/Discrete.h
@@ -0,0 +1,10 @@
+template<class T>
+vector<int> Discrete(vector<T>&v){
+	vector<int>ans;
+	vector<T>tmp(v);
+	sort(all(tmp));
+	tmp.erase(unique(all(tmp)),end(tmp));
+	for(auto i:v)
+		ans.pb(lower_bound(all(tmp),i)-tmp.begin()+1);
+	return ans;
+}
