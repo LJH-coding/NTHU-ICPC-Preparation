05529b89e5ee6fecd8dfaf51c0baac35287d214e
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-02-15 16:51:41 +0800

    rb_tree update

--- Codebook/Data-Structure/RedBlackTree.h
+++ Codebook/Data-Structure/RedBlackTree.h
@@ -4,7 +4,7 @@ template<class T, typename cmp=less<>>struct _tree{//#include<bits/extc++.h>
 	void insert(T x){st.insert({x,id++});}
 	void erase(T x){st.erase(st.lower_bound({x,0}));}
 	int order_of_key(T x){return st.order_of_key(*st.lower_bound({x,0}));}
-	T find_by_order(T x){return st.find_by_order(x)->first;}
+	T find_by_order(int x){return st.find_by_order(x)->first;}
 	T lower_bound(T x){return st.lower_bound({x,0})->first;}
 	T upper_bound(T x){return st.upper_bound({x,(int)1e9+7})->first;}
 	T smaller_bound(T x){return (--st.lower_bound({x,0}))->first;}
