6bca2aae10492343fbf2a49786940b0782b034b0
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-02-28 22:06:05 +0800

    update

--- Codebook/Misc/Discrete.h
+++ Codebook/Misc/Discrete.h
@@ -1,10 +1,9 @@
 template<class T>
-vector<int> Discrete(vector<T>&v){
+vector<int> Discrete(const vector<T>&v){
 	vector<int>ans;
 	vector<T>tmp(v);
 	sort(all(tmp));
 	tmp.erase(unique(all(tmp)),end(tmp));
-	for(auto i:v)
-		ans.pb(lower_bound(all(tmp),i)-tmp.begin()+1);
+	for(auto i:v)ans.pb(lower_bound(all(tmp),i)-tmp.begin()+1);
 	return ans;
 }
