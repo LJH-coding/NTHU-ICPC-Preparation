c37493042771fc404d6e9c1324a912cbb8e488cc
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-02-28 22:01:13 +0800

    update

--- Codebook/Data-Structure/Segtree.h
+++ Codebook/Data-Structure/Segtree.h
@@ -1,37 +1,31 @@
 template<class T>struct segment_tree{
-	int n;
 	struct node{
-		int l,r;
+		int l,r,mx_id,mn_id;
 		T lz,lz2;
 		T sum,mx,mn;
 		node(){
-			l = r = lz  = lz2 = sum = mx = mn = 0;
+			l = r = lz  = lz2 = sum = mx = mn = 0,mx_id = -1,mn_id = -1;
 		}
 		friend bool operator ==(node a,node b){
 			return (a.l==b.l and a.r==b.r);
 		}
 	};
 	vector<node>arr;
-	segment_tree(int _n) : n(_n) {
-		arr.assign(n<<2,node());
-	}
-	void pull(node &ans,node left,node right){
-		if(left==node()){
-			ans = right;
-			return;
-		}
-		if(right==node()){
-			ans = left;
-			return;
-		}
+	inline void pull(node &ans,const node &left,const node &right){
+		if(left==node())ans = right;
+		else if(right==node())ans = left;
+		else{
 		ans.mx = max(left.mx,right.mx);
+		ans.mx_id = (left.mx>right.mx?left.mx_id:right.mx_id);
 		ans.mn = min(left.mn,right.mn);
+		ans.mn_id = (left.mn<right.mn?left.mn_id:right.mn_id);
 		ans.sum = left.sum+right.sum;
 		ans.l = left.l,ans.r = right.r;
-	}void pull(int idx){
+		}
+	}inline void pull(const int &idx){
 		pull(arr[idx],arr[idx<<1],arr[idx<<1|1]);
 	}
-	void push(int idx){
+	inline void push(const int &idx){
 		int m = (arr[idx].l+arr[idx].r)>>1;
 		if(arr[idx].lz){
 			arr[idx<<1].mn = arr[idx<<1].mx = arr[idx].lz;
@@ -43,18 +37,19 @@ template<class T>struct segment_tree{
 			arr[idx<<1|1].lz = arr[idx].lz;
 			arr[idx<<1|1].lz2 = 0;
 			arr[idx].lz = 0;
+			arr[idx].lz2 = 0;
 		}
 		else if(arr[idx].lz2){
-			arr[idx<<1].mn += arr[idx].lz;
-			arr[idx<<1].mx += arr[idx].lz;
+			arr[idx<<1].mn += arr[idx].lz2;
+			arr[idx<<1].mx += arr[idx].lz2;
 			arr[idx<<1].sum+=(m-arr[idx].l+1)*arr[idx].lz2;
 			if(arr[idx<<1].lz==0)arr[idx<<1].lz2+=arr[idx].lz2;
 			else{
 				arr[idx<<1].lz+=arr[idx].lz2;
 				arr[idx<<1].lz2 = 0;
 			}
-			arr[idx<<1|1].mn += arr[idx].lz;
-			arr[idx<<1|1].mx += arr[idx].lz;
+			arr[idx<<1|1].mn += arr[idx].lz2;
+			arr[idx<<1|1].mx += arr[idx].lz2;
 			arr[idx<<1|1].sum+=(arr[idx].r-m)*arr[idx].lz2;
 			if(arr[idx<<1|1].lz==0)arr[idx<<1|1].lz2+=arr[idx].lz2;
 			else{
@@ -64,21 +59,20 @@ template<class T>struct segment_tree{
 			arr[idx].lz2 = 0;
 		}
 	}
-	void build(const vector<T>&v,int l,int r,int idx){
+	inline void build(const vector<T>&v,const int &l,const int &r,int idx = 1){
+		if(idx==1)arr.assign((r-l+1)<<2,node());
 		if(l==r){
 			arr[idx].sum = arr[idx].mn = arr[idx].mx = v[l];
-			arr[idx].l = arr[idx].r = l;
-			arr[idx].lz = arr[idx].lz2 = 0;
+			arr[idx].l = arr[idx].r = arr[idx].mx_id = arr[idx].mn_id = l;
 			return;
 		}
 		int m = (l+r)>>1;
 		build(v,l,m,idx<<1);
 		build(v,m+1,r,idx<<1|1);
 		pull(idx);
-	}void build(const vector<T>&v,int r){
-		build(v,1,r,1);
 	}
-	void set(int l,int r,T y,int idx = 1){
+	inline void set(const int &l,const int &r,const T &y,int idx = 1){
+		if(l>r)return;
 		if(l<=arr[idx].l and arr[idx].r<=r){
 			arr[idx].sum = (arr[idx].r-arr[idx].l+1)*y;
 			arr[idx].mn = arr[idx].mx = y;
@@ -92,7 +86,8 @@ template<class T>struct segment_tree{
 		if(r>m)set(l,r,y,idx<<1|1);
 		pull(idx);
 	}
-	void add(int l,int r,T y,int idx = 1){
+	inline void add(const int &l,const int &r,const T &y,int idx = 1){
+		if(l>r)return;
 		if(l<=arr[idx].l and arr[idx].r<=r){
 			arr[idx].mn += y;
 			arr[idx].mx += y;
@@ -107,13 +102,14 @@ template<class T>struct segment_tree{
 		if(r>m)add(l,r,y,idx<<1|1);
 		pull(idx);
 	}
-	node query(int l,int r,int idx = 1){
+	inline node query(const int &l,const int &r,int idx = 1){
+		if(l>r)return node();
 		if(l<=arr[idx].l and arr[idx].r<=r){
 			return arr[idx];
 		}
 		push(idx);
 		int m = (arr[idx].l+arr[idx].r)>>1;
-		node ans,left,right;
+		node ans = node(),left = node(),right = node();
 		if(l<=m)left = query(l,r,idx<<1);
 		if(r>m)right = query(l,r,idx<<1|1);
 		pull(ans,left,right);
