965c66801708d01c37eb06d63f08568efb276279
李佳樺 <leejiahua@lijiahuadeMacBook-Air.local>
2023-03-11 18:33:48 +0800

    Linear update

--- Codebook/Math/LinearSieve.h
+++ Codebook/Math/LinearSieve.h
@@ -1,20 +1,20 @@
-vector<bool> isprime;
+vector<bool> is_prime;
 vector<int> primes, phi, mobius;
 void linear_sieve(int n) {
 	n += 1;
-	isprime.resize(n);
-	fill(2 + ALL(isprime), true);
+	is_prime.resize(n);
+	fill(2 + begin(is_prime),end(is_prime), true);
 	phi.resize(n); mobius.resize(n);
 	phi[1] = mobius[1] = 1;
 	for(int i = 2; i < n; ++i) {
-		if(isprime[i]) {
-			primes.PB(i);
+		if(is_prime[i]) {
+			primes.push_back(i);
 			phi[i] = i - 1;
 			mobius[i] = -1;
 		}
 		for(auto j : primes) {
 			if(i * j >= n) break;
-			isprime[i * j] = false;
+			is_prime[i * j] = false;
 			if(i % j == 0) {
 				mobius[i * j] = 0;
 				phi[i * j] = phi[i] * j;
