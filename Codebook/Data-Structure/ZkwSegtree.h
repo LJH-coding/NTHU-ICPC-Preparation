1a9ae8ca50da87546dd624b5abd68553e6ebea01
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-04-16 19:48:53 +0800

    segment tree update

--- Codebook/Data-Structure/ZkwSegtree.h
+++ Codebook/Data-Structure/ZkwSegtree.h
@@ -45,6 +45,7 @@ public:
 	}
 	S query(int l, int r) {
 		assert(0 <= l && l <= r && r <= n);
+		r++;
 		if(l == r) {
 			return node_init();
 		}
@@ -84,6 +85,7 @@ public:
 	}
 	void update(int l, int r, F f) {
 		assert(0 <= l && l <= r && r <= n);
+		r++;
 		if(l == r) {
 			return;
 		}
