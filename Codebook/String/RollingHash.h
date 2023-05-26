2ef36b6cadc8f297b8e8e1c54ab1418265a4845f
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-05-26 11:18:47 +0800

    rolling hash update

--- Codebook/String/RollingHash.h
+++ Codebook/String/RollingHash.h
@@ -1,63 +1,11 @@
 template<int HASH_COUNT>
-class Hash {
-public:
-	Hash() : Hash("") {}
-
-	Hash(const string& s) : n(s.size()) {
-		//static_assert(HASH_COUNT > 0 && HASH_COUNT <= MAX_HASH_PAIRS);
-		for(int i = 0; i < HASH_COUNT; ++i) {
-			const auto& p = HASH_PAIRS[i];
-			pref[i].resize(n);
-			pref[i][0] = s[0];
-			for(int j = 1; j < n; ++j) {
-				pref[i][j] = (1LL * pref[i][j - 1] * p.first + s[j]) % p.second;
-			}
-		}
-		build_powers(n);
-	}
-
-	void add_char(char c) {
-		for(int i = 0; i < HASH_COUNT; ++i) {
-			const auto& p = HASH_PAIRS[i];
-			pref[i].push_back((1LL * (n == 0 ? 0 : pref[i].back()) * p.first + c) % p.second);
-		}
-		n += 1;
-		build_powers(n);
-	}
-
-	// Return hash values for [l, r)
-	array<int, HASH_COUNT> substr(int l, int r) {
-		array<int, HASH_COUNT> res{};
-		for(int i = 0; i < HASH_COUNT; ++i) {
-			res[i] = substr(i, l, r);
-		}
-		return res;
-	}
-
-	array<int, HASH_COUNT> merge(const vector<pair<int, int>>& seg) {
-		array<int, HASH_COUNT> res{};
-		for(int i = 0; i < HASH_COUNT; ++i) {
-			const auto& p = HASH_PAIRS[i];
-			for(auto [l, r] : seg) {
-				res[i] = (1LL * res[i] * get_power(i, r - l) + substr(i, l, r)) % p.second;
-			}
-		}
-		return res;
-	}
-
-
-
-	inline int size() const {
-		return n;
-	}
-
-private:
+struct RollingHash {
+	
 	static const int MAX_HASH_PAIRS = 10;
 
 	// {mul, mod}
 	const vector<pair<int, int>> HASH_PAIRS = {{827167801, 999999937}, {998244353, 999999929}, {146672737, 922722049}, {204924373, 952311013}, {585761567, 955873937}, {484547929, 901981687}, {856009481, 987877511}, {852853249, 996724213}, {937381759, 994523539}, {116508269, 993179543}};
-
-
+	
 	int n;
 	vector<int> POW[MAX_HASH_PAIRS];
 	array<vector<int>, HASH_COUNT> pref;
@@ -99,5 +47,53 @@ private:
 	int get_power(int a, int b) {
 		build_powers(b);
 		return POW[a][b];
+	}
+
+	RollingHash() : RollingHash("") {}
+
+	RollingHash(const string& s) : n(s.size()) {
+		//static_assert(HASH_COUNT > 0 && HASH_COUNT <= MAX_HASH_PAIRS);
+		for(int i = 0; i < HASH_COUNT; ++i) {
+			const auto& p = HASH_PAIRS[i];
+			pref[i].resize(n);
+			pref[i][0] = s[0];
+			for(int j = 1; j < n; ++j) {
+				pref[i][j] = (1LL * pref[i][j - 1] * p.first + s[j]) % p.second;
+			}
+		}
+		build_powers(n);
+	}
+
+	void add_char(char c) {
+		for(int i = 0; i < HASH_COUNT; ++i) {
+			const auto& p = HASH_PAIRS[i];
+			pref[i].push_back((1LL * (n == 0 ? 0 : pref[i].back()) * p.first + c) % p.second);
+		}
+		n += 1;
+		build_powers(n);
+	}
+
+	// Return hash values for [l, r)
+	array<int, HASH_COUNT> substr(int l, int r) {
+		array<int, HASH_COUNT> res{};
+		for(int i = 0; i < HASH_COUNT; ++i) {
+			res[i] = substr(i, l, r);
+		}
+		return res;
+	}
+
+	array<int, HASH_COUNT> merge(const vector<pair<int, int>>& seg) {
+		array<int, HASH_COUNT> res{};
+		for(int i = 0; i < HASH_COUNT; ++i) {
+			const auto& p = HASH_PAIRS[i];
+			for(auto [l, r] : seg) {
+				res[i] = (1LL * res[i] * get_power(i, r - l) + substr(i, l, r)) % p.second;
+			}
+		}
+		return res;
+	}
+
+	inline int size() const {
+		return n;
 	}
 };
